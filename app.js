a8ab72a177b90626d8c9ae3fb6c0917268b9aa9f
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-24 15:44:14 +0530

    PW API but un-offical (works)

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.set('views', './views/');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const url = "https://api.penpencil.xyz";
+
+// my access token dont use uwu
+let access_token = ""; // 34aebd1f67ec8352257b0e2c5489122b193d73ab54449f3be4833e0f06c239b4
+
+app.get("/", (req,res)=>{
+    res.render("home")
+})
+
+app.post("/", (req,res)=>{
+    access_token = req.body.access;
+    console.log(access_token)
+    // res.redirect("/data")
+})
+
+app.get("/:token/data", (req, res) => {
+    const aToken = req.params.token;
+    let config = {
+        headers: {
+            authorization: `Bearer ${aToken}`
+        },
+        params: {
+            mode: 1
+        }
+    }
+    axios.get(`${url}/v3/batches/my-batches`, config)
+    .then(response => {
+        res.send(response.data.data);
+    })
+});
+
+app.listen(process.env.PORT || 6969, ()=>{
+    console.log("Running on port 6969")
+})
